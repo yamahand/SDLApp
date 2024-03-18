0a8430afc04c182358cd4a09b54fcd36e6082d29
yamahand <a18293372@gmail.com>
2024-03-19 01:20:29 +0900

    Application追加

---
+++ lib/src/Application/Application.cpp
@@ -0,0 +1,41 @@
+﻿#include "Application.h"
+
+Application::Application(int argc, char* argv[])
+{
+}
+
+Application::~Application()
+{
+}
+
+bool Application::Initialize()
+{
+	return OnInitialize();
+}
+
+bool Application::Update()
+{
+	return OnUpdate();
+}
+
+bool Application::Finazlie()
+{
+	return OnFinalize();
+}
+
+
+void Application::Run()
+{
+	bool isSuccess = Initialize();
+	if (!isSuccess) {
+		return;
+	}
+
+	while (isSuccess) {
+		isSuccess = Update();
+	}
+
+	Finazlie();
+}
+
+
---
+++ lib/src/Application/Application.h
@@ -0,0 +1,51 @@
+﻿
+class Application {
+protected:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="argc">引数の数</param>
+	/// <param name="argv">引数</param>
+	Application(int argc, char* argv[]);
+
+	Application() = delete;
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+	Application(Application&&) = delete;
+	Application() = delete;
+	Application& operator=(Application&&) = delete;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	virtual ~Application();
+
+public:
+	void Run();
+
+protected:
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	/// <returns>true:成功 false:失敗</returns>
+	virtual bool OnInitialize() = 0;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	/// <returns>true:更新継続 false:終了</returns>
+	virtual bool OnUpdate() = 0;
+
+	/// <summary>
+	/// 終了処理
+	/// </summary>
+	/// <returns>true:成功 false:失敗</returns>
+	virtual bool OnFinalize() = 0;
+
+private:
+	bool Initialize();
+
+	bool Update();
+
+	bool Finazlie();
+};
