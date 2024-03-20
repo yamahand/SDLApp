9a4d565d16d06d4a8cfe5666c42cf89af71a878e
yamahand <a18293372@gmail.com>
2024-03-20 13:26:01 +0900

    imgui用premake追加

---
+++ external/imgui.lua
@@ -0,0 +1,30 @@
+group("external")
+project("imgui")
+  kind("StaticLib")
+  language("C++")
+  links({
+  })
+  defines({
+    "_LIB",
+  })
+  includedirs({
+    "imgui",
+    "SDL/include",
+  })
+  files({
+    "imgui/imconfig.h",
+    "imgui/imgui_demo.cpp",
+    "imgui/imgui_draw.cpp",
+    "imgui/imgui_internal.h",
+    "imgui/imgui_tables.cpp",
+    "imgui/imgui_widgets.cpp",
+    "imgui/imgui.cpp",
+    "imgui/imgui.h",
+    "imgui/imstb_rectpack.h",
+    "imgui/imstb_textedit.h",
+    "imgui/imstb_truetype.h",
+    "imgui/backends/imgui_impl_sdl3.cpp",
+    "imgui/backends/imgui_impl_sdl3.h",
+    "imgui/backends/imgui_impl_sdlrenderer3.cpp",
+    "imgui/backends/imgui_impl_sdlrenderer3.h",
+  })
