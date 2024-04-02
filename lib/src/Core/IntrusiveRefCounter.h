551db9995fdc67fe083d63232a769455c2193321
yamahand <a18293372@gmail.com>
2024-04-02 13:53:10 +0900

    w.i.p

---
+++ lib/src/Core/IntrusiveRefCounter.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+template <class DeriveT>
+class IntrusiveRefCounter {
+public:
+    IntrusiveRefCounter()                                   = noexcept;
+    IntrusiveRefCounter(const IntrusiveRefCounter const& r) = noexcept;
+    IntrusiveRefCounter& operator=(IntrusiveRefCounter const& r) noexcept;
+
+    uint32_t UseCount() const noexcept;
+
+protected:
+    ~IntrusiveRefCounter() = default;
+};
