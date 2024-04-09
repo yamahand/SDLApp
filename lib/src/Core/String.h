ed703564b87042a6b4d90fadf9fa9ac42e088708
yamahand <a18293372@gmail.com>
2024-04-10 08:56:01 +0900

    文字列追加
    未実装でstdをusingしているだけ

---
+++ lib/src/Core/String.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <string>
+
+namespace lib {
+using U8String = std::u8string;
+using U8StringView = std::u8string_view;
+using U16String = std::u16string;
+using U16StringView = std::u16string_view;
+using U32String = std::u32string;
+using U32StringView = std::u32string_view;
+
+using String = U8String;
+using StringView = U8StringView;
+}
