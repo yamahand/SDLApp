a4553adc56229637ef60f64235b1ff0fd827edab
yamahand <a18293372@gmail.com>
2024-09-15 00:53:45 +0900

    Logger追加

---
+++ lib/src/Logger/Logger.cpp
@@ -0,0 +1,16 @@
+﻿#include "Logger/Logger.h"
+
+#include <cstdarg>
+
+namespace lib {
+void Logger::Initialize() {
+}
+void Logger::Finalize() {
+}
+void Logger::Trace(Level level, const char* category, const char* format, ...) {
+	va_list args;
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+}
+}  // namespace lib
---
+++ lib/src/Logger/Logger.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "Config/Config.h"
+#include "Core/Singleton.h"
+
+
+#if defined(PB_TRACE)
+#define LOG_TRACE(category, ...) GetLogger().Trace(Logger::Level::Trace, category, __VA_ARGS__)
+#define LOG_INFO(category, ...) GetLogger().Trace(Logger::Level::Info, category, __VA_ARGS__)
+#define LOG_WARN(category, ...) GetLogger().Trace(Logger::Level::Warn, category, __VA_ARGS__)
+#define LOG_ERROR(category, ...) GetLogger().Trace(Logger::Level::Error, category, __VA_ARGS__)
+#else
+#define LOG_TRACE(category, ...)
+#define LOG_INFO(category, ...)
+#define LOG_WARN(category, ...)
+#define LOG_ERROR(category, ...)
+#endif
+
+namespace lib {
+
+class Logger {
+public:
+	enum class Level {
+		Trace,
+		Info,
+		Warn,
+		Error,
+	};
+public:
+    Logger() = default;
+	~Logger() = default;
+
+	void Initialize();
+	void Finalize();
+
+	void Trace(Level level, const char* category, const char* format, ...);
+};
+
+static Logger& GetLogger() {
+	return Singleton<Logger>::GetInstance();
+}
+
+}  // namespace lib
