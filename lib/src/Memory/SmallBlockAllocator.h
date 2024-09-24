ddb64153ba241edb28d414413655acd17b5df365
yamahand <a18293372@gmail.com>
2024-09-24 23:55:28 +0900

    SmallBlockAllocator追加

---
+++ lib/src/Memory/SmallBlockAllocator.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Memory/MemoryAllocator.h"
+#include <bitset>
+
+namespace lib {
+
+template<size_t BlockSize>
+class SmallBlockAllocator : public MemoryAllocator {
+public:
+    SmallBlockAllocator(uintptr_t memoryAddress, size_t memorySize)
+        : MemoryAllocator(memoryAddress, memorySize) {}
+
+    void* Allocate(size_t size) override;
+    void Deallocate(void* ptr) override;
+
+    constexpr size_t GetBlockSize() const { return BlockSize; }
+
+private:
+    std::bitset<256> m_freeBlockBit;
+};
+
+}  // namespace lib
