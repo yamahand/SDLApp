2557836eabb867609e50c806ea3b76f16dc35779
yamahand <a18293372@gmail.com>
2024-10-07 08:54:23 +0900

    assert追加

---
+++ lib/src/Core/Assert.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Logger/Logger.h"
+
+#define PB_ASSERT(expr) do { if (!(expr)) { __debugbreak(); } } while (0)
+#define PB_ASSERT_MSG(expr, msg) do { if (!(expr)) { LOG_ERROR("assert", msg); __debugbreak(); } } while (0)
+#define PB_ASSERT_MSG(expr, category, msg) do { if (!(expr)) { LOG_ERROR(category, msg); __debugbreak(); } } while (0)
