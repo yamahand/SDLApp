641596a9d730179c643830ef8d47fe88870bc2e4
yamahand <a18293372@gmail.com>
2024-10-12 12:33:26 +0900

    Spriteにサイズ操作API追加

--- lib/src/Gfx/Sprite.h
+++ lib/src/Gfx/Sprite.h
@@ -42,6 +42,28 @@ public:
         return m_height;
     }
 
+    /*math::Vector4 GetSize() const {
+        return math::Vector4(static_cast<float>(m_width), static_cast<float>(m_height), 0.0f, 0.0f);
+    }*/
+
+    void SetSize(const size_t width, const size_t height) {
+        m_width  = width;
+        m_height = height;
+    }
+
+    void SetSize(const math::Vector4& size) {
+        m_width  = static_cast<size_t>(size.m128_f32[0]);
+        m_height = static_cast<size_t>(size.m128_f32[1]);
+    }
+
+    void SetWidth(const size_t width) {
+        m_width = width;
+    }
+
+    void SetHeight(const size_t height) {
+        m_height = height;
+    }
+
 protected:
     math::Vector4 m_position;
     size_t m_width  = 0;
