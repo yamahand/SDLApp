817f96ada3f077608c5dbdb95f6c049671f48bd4
yamahand <a18293372@gmail.com>
2024-10-14 11:14:01 +0900

    Microsoft/SimpleMath追加

--- lib/src/Math/Math.h
+++ lib/src/Math/Math.h
@@ -1,7 +1,14 @@
 ﻿#include "DirectXMath/Inc/DirectXMath.h"
+#include "Math/SimpleMath.h"
 
 namespace math {
-using Vector4 = DirectX::XMVECTOR;
-using Matrx4x4 = DirectX::XMMATRIX;
-using Quaternion = DirectX::XMVECTOR;
+using Vector4 = DirectX::SimpleMath::Vector4;
+using Vector3 = DirectX::SimpleMath::Vector3;
+using Vector2 = DirectX::SimpleMath::Vector2;
+using Matrx = DirectX::SimpleMath::Matrix;
+using Quaternion = DirectX::SimpleMath::Quaternion;
+using Color = DirectX::SimpleMath::Color;
+using Ray = DirectX::SimpleMath::Ray;
+using Plane = DirectX::SimpleMath::Plane;
+using Viewport = DirectX::SimpleMath::Viewport;
 }
