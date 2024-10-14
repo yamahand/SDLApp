04db40987e646f7a114c12330996b87a948e6421
yamahand <a18293372@gmail.com>
2024-10-14 11:15:53 +0900

    Assert修正

--- lib/src/Core/Assert.h
+++ lib/src/Core/Assert.h
@@ -1,8 +1,35 @@
 #pragma once
 
-#include "Logger/Logger.h"
 #include "Logger/LogLevel.h"
+#include "Logger/Logger.h"
 
-#define PB_ASSERT(expr) do { if (!(expr)) { __debugbreak(); } } while (0)
-//#define PB_ASSERT_MSG(expr, msg) do { if (!(expr)) { LOG_ERROR("assert", msg); __debugbreak(); } } while (0)
-#define PB_ASSERT_MSG(expr, category, msg) do { if (!(expr)) { LOG_ERROR(category, msg); __debugbreak(); } } while (0)
+#define PB_ASSERT(expr)                \
+    do {                               \
+        if (!(expr)) {                 \
+            LOG_ERROR("asert", #expr); \
+            __debugbreak();            \
+        }                              \
+    } while (0)
+
+#define PB_ASSERT_MSG(expr, category, msg) \
+    do {                                   \
+        if (!(expr)) {                     \
+            LOG_ERROR(category, msg);      \
+            __debugbreak();                \
+        }                                  \
+    } while (0)
+
+#define PB_WARNING(expr)              \
+    do {                              \
+        if (!expr) {                  \
+            LOG_WARN("waring", #expr) \
+        }                             \
+    }
+
+#define PB_WARNING_MSG(expr, category, msg) \
+    do {                                    \
+        if (!(expr)) {                      \
+            LOG_WARN(category, msg);        \
+            __debugbreak();                 \
+        }                                   \
+    } while (0)
