06082fe30fcc4d6096b33d05d4ece00d23929f62
yamahand <a18293372@gmail.com>
2025-03-08 16:06:27 +0900

    submodule更新

--- external/SDL3.lua
+++ external/SDL3.lua
@@ -1,5 +1,2 @@
-SDL_INC_DIR = "external/DirectXMath/Inc"
-SDL_LIB_DIR = "external/DirectXMath/Extensions"
-
 SDL_INC_DIR = "external/SDL3/include"
 SDL_LIB_DIR = "external/SDL3/VisualC/SDL/%{cfg.platform}/%{cfg.buildcfg}"
